6ac5ca42a6303524f84f3b4850197fbf2e026c11
Fredrik Johansson <fjohansson.creative@gmail.com>
2023-10-29 15:49:24 +0100

    fix: made image not scale with height

--- src/components/contacts/person.tsx
+++ src/components/contacts/person.tsx
@@ -3,7 +3,7 @@ import type { Contact } from "~/db"
 
 export const Person = component$((props: Contact) => {
   return (
-    <article class="bg-gray-100 p-4 shadow rounded flex gap-2">
+    <article class="bg-gray-100 p-4 shadow rounded flex items-start gap-2">
       <img class="rounded-md" width={50} height={50} src={props.photo} />
       <div>
         <h2>{props.name}</h2>
